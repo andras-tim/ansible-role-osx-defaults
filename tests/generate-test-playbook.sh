77718177f3a504cf34b71a88ac34f2ea904d9961
Andras Tim <andras.tim@gmail.com>
2020-05-19 02:16:27 +0200

    travis: Added real testcases to be able to test the functionality
    Thank you for geerlingguy to be able to pick some idea from
    https://github.com/geerlingguy/ansible-role-homebrew/blob/f2e1d07134c0f35bc93779c73b36b2b32104d620/.travis.yml

---
+++ tests/generate-test-playbook.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -eufo pipefail
+
+function main()
+{
+    cd "$(dirname "$0")"
+    {
+        get_header
+        if [ "${1-:}" == '--enable-tasks' ]; then
+            get_variables
+        fi
+    } >test.yml
+}
+
+function get_header()
+{
+cat - <<EOF
+---
+
+- hosts: localhost
+  remote_user: root
+  roles:
+    - role: ansible-role-osx-defaults
+EOF
+}
+
+function get_variables()
+{
+    echo '      vars:'
+    grep -i '_Enabled:' '../defaults/main.yml' \
+        | sed -e 's|: false.*|: true|' -e 's|^|        |'
+}
+
+
+main "$@"
